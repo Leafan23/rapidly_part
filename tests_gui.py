32685a83afc7f795d7c8b7ce7e9130e27d538823
Leafan <la2player_07@mail.ru>
2023-12-01 00:29:32 +0300

    v0.3 Добавлена обратная отправка данных

--- tests_gui.py
+++ tests_gui.py
@@ -7,6 +7,11 @@ class App(customtkinter.CTk):
         self.geometry("750x700")
 
         self.new_window = []
+        self.int = customtkinter.IntVar()
+        self.int.set(0)
+
+        self.lable = customtkinter.CTkLabel(self, text=self.int.get())
+        self.lable.pack()
 
         self.button = customtkinter.CTkButton(self, text="Создать окно", command=self.add_window)
         #  self.field_delete.configure(state="disabled")
@@ -24,4 +29,8 @@ class NewWindow(customtkinter.CTkToplevel):
         self.button.pack()
 
     def add_int(self):
-        pass
+        #  self.i = self.master.int.get()
+        self.button_in_main = customtkinter.CTkButton(self.master, text="Новая кнопка", command=None)
+        self.button_in_main.pack()
+
+
